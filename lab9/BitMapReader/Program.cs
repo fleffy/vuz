e0407a5d8ef8e481d703d3507edc10d33a98d545
Yevhenii Sivukhin <84014406+fleffy@users.noreply.github.com>
2023-04-11 10:59:07 +0300

    Add files via upload

---
+++ lab9/BitMapReader/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Drawing;
+
+namespace BitMapReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagesPath = @"C:\Images\";
+
+            Func<Bitmap, Bitmap> grayscaleFunc = Grayscale;
+            Func<Bitmap, Bitmap> rotate90Func = Rotate90;
+
+            Func<Bitmap, Bitmap> processingFunc = grayscaleFunc;
+            processingFunc += rotate90Func;
+
+            Action<Bitmap> displayAction = Display;
+
+            string[] imageFiles = Directory.GetFiles(imagesPath);
+            foreach (string imageFile in imageFiles)
+            {
+                Bitmap originalImage = new Bitmap(imageFile);
+                Bitmap processedImage = processingFunc(originalImage);
+                displayAction(processedImage);
+            }
+        }
+
+        static Bitmap Grayscale(Bitmap originalImage)
+        {
+            Bitmap processedImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color pixel = originalImage.GetPixel(x, y);
+                    int grayScale = (int)((pixel.R * 0.3) + (pixel.G * 0.59) + (pixel.B * 0.11));
+                    Color grayColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                    processedImage.SetPixel(x, y, grayColor);
+                }
+            }
+
+            return processedImage;
+        }
+
+        static Bitmap Rotate90(Bitmap originalImage)
+        {
+            Bitmap processedImage = new Bitmap(originalImage.Height, originalImage.Width);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color pixel = originalImage.GetPixel(x, y);
+                    processedImage.SetPixel(y, originalImage.Width - x - 1, pixel);
+                }
+            }
+
+            return processedImage;
+        }
+
+        static void Display(Bitmap image)
+        {
+            int width = image.Width;
+            int height = image.Height;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+
+                    if (pixelColor.GetBrightness() < 0.5)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ lab9/JSONReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace JSONReader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToDirectory = @"path";
+
+            Func<Product, bool> filter = p => p.Price > 50 && p.Category == "Electronics";
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string filePath = Path.Combine(pathToDirectory, $"{i}.json");
+
+                var products = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText(filePath));
+                List<Product> filteredProducts = products.Where(filter).ToList();
+                filteredProducts.ForEach(p => Console.WriteLine(p.Name));
+            }
+        }
+    }
+}
